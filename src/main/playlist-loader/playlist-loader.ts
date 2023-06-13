2f6b0baa7e41d5367deb650e01ec991854bd94ce
Vincent (Anapher) <anapher@protonmail.com>
2023-06-13 23:53:33 +0200

    is fast-glob working?

--- src/main/playlist-loader/playlist-loader.ts
+++ src/main/playlist-loader/playlist-loader.ts
@@ -1,4 +1,7 @@
-import { M3uTrack } from './m3u-parser';
+import glob from 'fast-glob';
+import fs from 'fs/promises';
+import path from 'node:path';
+import parseM3uFile, { M3uTrack } from './m3u-parser';
 
 export type M3uPlaylist = {
   path: string;
@@ -8,31 +11,29 @@ export type M3uPlaylist = {
 export default async function loadAllPlaylistsFromDirectory(
   directory: string
 ): Promise<M3uPlaylist[]> {
-  console.log(directory);
-  return [];
-  // const playlistFiles = await glob(`${directory}/**/*.m3u`);
+  const playlistFiles = await glob(`${directory}/**/*.m3u`);
 
-  // const playlistStrings = await Promise.all(
-  //   playlistFiles.map(
-  //     async (p) => [p, await fs.readFile(p, 'utf8')] as [string, string]
-  //   )
-  // );
+  const playlistStrings = await Promise.all(
+    playlistFiles.map(
+      async (p) => [p, await fs.readFile(p, 'utf8')] as [string, string]
+    )
+  );
 
-  // return playlistStrings.map((x) => {
-  //   const [filePath, content] = x;
-  //   const tracks = parseM3uFile(content);
-  //   const playlistDir = path.dirname(filePath);
+  return playlistStrings.map((x) => {
+    const [filePath, content] = x;
+    const tracks = parseM3uFile(content);
+    const playlistDir = path.dirname(filePath);
 
-  //   return {
-  //     path: filePath,
-  //     tracks: tracks.map((track) => ({
-  //       ...track,
-  //       path: path.isAbsolute(track.path)
-  //         ? track.path
-  //         : path.normalize(
-  //             `${playlistDir}/${track.path.replaceAll('\\', '/')}`
-  //           ),
-  //     })),
-  //   };
-  // });
+    return {
+      path: filePath,
+      tracks: tracks.map((track) => ({
+        ...track,
+        path: path.isAbsolute(track.path)
+          ? track.path
+          : path.normalize(
+              `${playlistDir}/${track.path.replaceAll('\\', '/')}`
+            ),
+      })),
+    };
+  });
 }
