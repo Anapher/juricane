3dd2a3adca5f7f9fd60121eeb62449dd2cf94a2d
Vincent (Anapher) <anapher@protonmail.com>
2023-10-09 23:08:03 +0200

    fix path normalization

--- src/main/path-utils.ts
+++ src/main/path-utils.ts
@@ -2,8 +2,8 @@ import path from 'node:path';
 
 export function normalizePath(p: string, baseDir: string) {
   return path.isAbsolute(p)
-    ? p.replaceAll('\\', '/')
-    : path.normalize(`${baseDir}/${p}`).replaceAll('\\', '/');
+    ? path.normalize(p.replaceAll('\\', '/'))
+    : path.normalize(`${baseDir}/${p}`.replaceAll('\\', '/'));
 }
 
 export default null;
