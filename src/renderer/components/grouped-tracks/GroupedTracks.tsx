256f2e584b8bcbc806407898864cad1675779668
Vincent (Anapher) <anapher@protonmail.com>
2023-10-21 15:30:35 +0200

    virtualize group pages

--- src/renderer/components/grouped-tracks/GroupedTracks.tsx
+++ src/renderer/components/grouped-tracks/GroupedTracks.tsx
@@ -1,13 +1,19 @@
+/* eslint-disable react/no-unstable-nested-components */
 import {
+  Box,
   Card,
   CardActionArea,
   CardContent,
   CardMedia,
-  Grid,
   Typography,
 } from '@mui/material';
+import _ from 'lodash';
+import { useMemo } from 'react';
+import { Virtuoso } from 'react-virtuoso';
 import { CategoryInfo } from 'renderer/types';
 import TrackListPreview from '../track-list-preview/TrackListPreview';
+
+const GROUPS_PER_ROW = 6;
 
 type Props = {
   groups: CategoryInfo[];
@@ -15,37 +21,53 @@ type Props = {
 };
 
 export default function GroupedTracks({ groups, onNavigateToGroup }: Props) {
+  const chunks = useMemo(() => _.chunk(groups, GROUPS_PER_ROW), [groups]);
+
   return (
-    <Grid
-      flex={1}
-      container
-      spacing={2}
-      columns={{ xs: 4, md: 6, xl: 8 }}
-      overflow="auto"
-      m={0}
-      pr={2}
-      pb={2}
-    >
-      {groups.map((group) => (
-        <Grid item key={group.id} display="flex" xs={1} alignItems="flex-start">
-          <Card sx={{ flex: 1, minHeight: 0 }}>
-            <CardActionArea onClick={() => onNavigateToGroup(group)}>
-              <CardMedia>
-                <TrackListPreview images={group.previewImageTrackIds} />
-              </CardMedia>
-              <CardContent>
-                <Typography
-                  gutterBottom
-                  sx={{ fontSize: { xs: 14, xl: 18 } }}
-                  component="div"
-                >
-                  {group.name}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
+    <Virtuoso
+      style={{ flex: 1, width: '100%' }}
+      data={chunks}
+      itemContent={(__, chunk) => (
+        <Box display="flex" flexDirection="row">
+          {chunk.map((group) => (
+            <Card
+              key={group.id}
+              sx={{
+                flex: 1,
+                minHeight: 0,
+                m: 1,
+                display: 'flex',
+              }}
+            >
+              <CardActionArea
+                sx={{ flex: 1 }}
+                onClick={() => onNavigateToGroup(group)}
+              >
+                <CardMedia>
+                  <TrackListPreview images={group.previewImageTrackIds} />
+                </CardMedia>
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    sx={{ fontSize: { xs: 14, xl: 18 } }}
+                    component="div"
+                  >
+                    {group.name}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+          {chunk.length !== GROUPS_PER_ROW && (
+            <Box
+              sx={{
+                flex: GROUPS_PER_ROW - chunk.length,
+                mx: GROUPS_PER_ROW - chunk.length,
+              }}
+            />
+          )}
+        </Box>
+      )}
+    />
   );
 }
